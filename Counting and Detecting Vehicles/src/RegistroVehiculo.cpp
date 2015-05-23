8a35aa00da48fb95b84d656dc4d07fca484cb8e7
Alonso Loaiza <hiyitsu@gmail.com>
2015-05-22 22:23:26 -0600

    Agrega clase para el almacenamiento de información respecto al conteo de un vehiculo.

---
+++ Counting and Detecting Vehicles/src/RegistroVehiculo.cpp
@@ -0,0 +1,85 @@
+/*
+ * RegistroVehiculo.cpp
+ *
+ * @Author: Alonso Loaiza Pereira
+ * @e-mail: hiyitsu@gmail.com
+ * @University: Instituto Tecnologico de Costa Rica (TEC)
+ */
+
+#include "RegistroVehiculo.hpp"
+
+RegistroVehiculo::RegistroVehiculo()
+{
+	// TODO Auto-generated constructor stub
+	//Datos default
+	mFecha = time(0);	//Fecha Actual
+	mPlaca = "ABC123";
+	mVelocidadPromedio = 0;
+	mTipoVehiculo = "Liviano";
+}
+
+RegistroVehiculo::RegistroVehiculo(time_t rFecha, string rPlaca, float rVelocidad, string rTipo)
+{
+	// TODO Auto-generated constructor stub
+	mFecha = rFecha;	//Fecha Actual
+	mPlaca = rPlaca;
+	mVelocidadPromedio = rVelocidad;
+	mTipoVehiculo = rTipo;
+}
+
+RegistroVehiculo::~RegistroVehiculo()
+{
+	// TODO Auto-generated destructor stub
+}
+
+/*
+ *********************************************************************************
+ *                                 Getters                                       *
+ *********************************************************************************
+ */
+time_t RegistroVehiculo::getFecha()
+{
+	return mFecha;
+}
+
+string RegistroVehiculo::getPlaca()
+{
+	return mPlaca;
+}
+
+float  RegistroVehiculo::getVelocidadPromedio()
+{
+	return mVelocidadPromedio;
+}
+
+string RegistroVehiculo::getTipoVehiculo()
+{
+	return mTipoVehiculo;
+}
+
+/*
+ *********************************************************************************
+ *                                 Setters                                       *
+ *********************************************************************************
+ */
+void RegistroVehiculo::setFecha(time_t rFecha)
+{
+	mFecha = rFecha;
+}
+
+void RegistroVehiculo::setPlaca(string rPlaca)
+{
+	mPlaca = rPlaca;
+}
+
+void RegistroVehiculo::setVelocidadPromedio(float rVelocidad)
+{
+	mVelocidadPromedio = rVelocidad;
+}
+
+void RegistroVehiculo::setTipoVehiculo(string rTipo)
+{
+	mTipoVehiculo = rTipo;
+}
+
+
---
+++ Counting and Detecting Vehicles/src/RegistroVehiculo.hpp
@@ -0,0 +1,57 @@
+/*
+ * RegistroVehiculo.hpp
+ *
+ * @Author: Alonso Loaiza Pereira
+ * @e-mail: hiyitsu@gmail.com
+ * @University: Instituto Tecnologico de Costa Rica (TEC)
+ *
+ * @Description:
+ * Clase encargada de almacenar la informacion (Simple POJO) sobre el conteo de un vehiculo como lo es el caso
+ * de la fecha en la que se realizo la captura, la velocidad promedio del vehiculo, el tipo de vehiculo y su
+ * placa.
+ */
+
+//Bibliotecas
+#include <ctime>		//Manejo de Dates (time_t)
+#include <string>		//Manejo de Strings
+using namespace std;
+
+//Constantes
+int NAME = 3;
+
+#ifndef REGISTROVEHICULO_HPP_
+#define REGISTROVEHICULO_HPP_
+
+class RegistroVehiculo
+{
+
+public:
+	//Constructor y Destructor
+	RegistroVehiculo();
+	RegistroVehiculo(time_t rFecha, string rPlaca, float rVelocidad, string rTipo);
+	virtual ~RegistroVehiculo();
+
+	//Getters
+	time_t getFecha();
+	string getPlaca();
+	float  getVelocidadPromedio();
+	string getTipoVehiculo();
+
+	//Setters
+	void setFecha(time_t rFecha);
+	void setPlaca(string rPlaca);
+	void setVelocidadPromedio(float rVelocidad);
+	void setTipoVehiculo(string rTipo);
+
+private:
+
+	//Atributos
+	time_t mFecha;				//Fecha de la detección del vehiculo
+	string mPlaca;				//Placa del vehiculo
+	float  mVelocidadPromedio;	//Velocidad con la que paso el vehiculo por la estructura vial
+	string mTipoVehiculo;		//Tipo de vehiculo
+
+
+};
+
+#endif /* REGISTROVEHICULO_HPP_ */
