9925aee535464ae1c0035f6ccc0f31e76bbd09f5
Alonso Loaiza <hiyitsu@gmail.com>
2015-05-25 19:11:52 -0600

    Agrega clase security token como medio de verificacion de informacion a traves del webservice.

---
+++ Counting and Detecting Vehicles/src/SecurityToken.cpp
@@ -0,0 +1,58 @@
+/*
+ * SecurityToken.cpp
+ *
+ * @Author: Alonso Loaiza Pereira
+ * @e-mail: hiyitsu@gmail.com
+ * @University: Instituto Tecnologico de Costa Rica (TEC)
+ */
+
+#include "SecurityToken.hpp"
+
+
+/*
+ *********************************************************************************
+ *                           Constructor y Destructor                            *
+ *********************************************************************************
+ */
+SecurityToken::SecurityToken()
+{
+	// TODO Auto-generated constructor stub
+}
+
+SecurityToken::~SecurityToken()
+{
+	// TODO Auto-generated destructor stub
+}
+
+/*
+ *********************************************************************************
+ *                                 Getters                                       *
+ *********************************************************************************
+ */
+
+string SecurityToken::getUsername()
+{
+	return mUsername;
+}
+
+string SecurityToken::getPassword()
+{
+	return mPassword;
+}
+
+/*
+ *********************************************************************************
+ *                                 Setters                                       *
+ *********************************************************************************
+ */
+
+void SecurityToken::setUsername(string rUser)
+{
+	mUsername = rUser;
+}
+
+void SecurityToken::setPassword(string rPassword)
+{
+	mPassword = rPassword;
+}
+
