381aee6c0a40ea336ad0a7a206c72f414a9448da
Alonso Loaiza <hiyitsu@hotmail.com>
2015-10-02 13:32:27 -0600

    Create the main file

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MathLib.cpp
+// Author      : Alonso Loaiza Pereira
+// Version     :
+// Copyright   : GNU GPL 3.0
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! to everyone in this classroom" << endl; // prints !!!Hello World!!!
+	return 0;
+}
