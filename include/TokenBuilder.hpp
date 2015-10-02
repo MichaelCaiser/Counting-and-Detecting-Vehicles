4c8d25a1601dd71a4bbe95b9e11baff09754c64c
Alonso Loaiza <hiyitsu@gmail.com>
2015-10-02 15:14:32 -0600

    Adding the class TokenBuilder for the creation of tokens for security proposes. The class is in the namespace Security, and uses a .hpp file of constans (SecurityConstans.hpp).

---
+++ include/TokenBuilder.hpp
@@ -0,0 +1,49 @@
+/**
+ * @file		SecurityTokenBuilder.hpp
+ * @version 	1.0
+ * @author		Alonso Loaiza Pereira
+ * @e-mail		aloaizapereira@gmail.com
+ * @university 	Tecnológico de Costa Rica (TEC)
+ *
+ * @description
+ * 		This class creates security tokens, this tokens are necessary because all the information
+ * 		transmitted to the server will be checked, the token has a validity that depends of time
+ * 		(normally is about 5 minutes).
+ */
+
+#ifndef TOKENBUILDER_HPP_
+#define TOKENBUILDER_HPP_
+
+//Libraries
+#include <string>	/* Object string */
+#include <time.h>	/* time_t, struct tm, localtime, strftime */
+
+//Specification of objects by namespace
+using std::string;
+
+namespace Security
+{
+	class TokenBuilder {
+	public:
+		//Constructor and Destructor
+		TokenBuilder();
+		virtual ~TokenBuilder();
+
+		//Setters
+		void   setEncryptionKey(string rStringKey);
+
+		//Methods
+		string createToken();
+
+	private:
+		//Attributes
+		string mEncryptionKey;	//The key to encrypt token's information
+
+		//Methods
+		string encryptString(string rString);
+		string getCurrentDate();
+	};
+
+} /* namespace Security */
+
+#endif /* TOKENBUILDER_HPP_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 //============================================================================
-// Name        : MathLib.cpp
+// Name        : main.cpp
 // Author      : Alonso Loaiza Pereira
 // Version     :
-// Copyright   : GNU GPL 3.0
+// Copyright   : MIT License
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <stdlib.h>
 #include <iostream>
+#include <ctime>
+#include "TokenBuilder.hpp"
+
 using namespace std;
+using Security::TokenBuilder;
 
 int main() {
-	cout << "!!!Hello World!!! to everyone in this classroom" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	/*
+	 ************************************************************************************
+	 *								TEST 1.0 = Token generation							*
+	 ************************************************************************************
+     */
+	TokenBuilder* token_builder = new TokenBuilder();
+	token_builder->setEncryptionKey("comidacomida");
+	cout<< "Generated token: "<<token_builder->createToken()<<endl;
+
+	//Free memory
+	delete token_builder;
+
+	return EXIT_SUCCESS;
 }
